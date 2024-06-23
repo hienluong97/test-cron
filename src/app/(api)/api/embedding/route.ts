c94f1b0587b100466633bf611d7e327670598f0a
luong <hienluong1997@gmail.com>
2024-06-24 00:51:55 +0700

    add api

--- src/app/(api)/api/embedding/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
- 
-export async function POST(req: Request) {
-    const data = await req.json();
-
-    return NextResponse.json({ data })
-
-}
